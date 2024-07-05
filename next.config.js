91b8b727db65989fa01ee4bae602b2119c224039
Classic Values <classicvaluesllc@gmail.com>
2024-07-04 21:54:27 -0700

    COMMIT

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-
-};
+    webpack: (config, { isServer }) => {
+      // Exclude fs and module from being bundled in client-side builds
+      if (!isServer) {
+        config.externals = {
+          fs: 'empty',
+          module: 'empty',
+          v8: 'empty',
+        };
+      }
+  
+      return config;
+    },
+  };
+  
