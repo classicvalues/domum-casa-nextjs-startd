03b6024a82299f38553b54bcbe5df2de53b69a12
Classic Values <classicvaluesllc@gmail.com>
2024-07-04 18:14:31 -0700

    COMMIT

--- next.config.js
+++ next.config.js
@@ -3,10 +3,18 @@ const withSvgr = require('@svgr/webpack');
 module.exports = withSvgr({
   reactStrictMode: true,
   webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    try {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+    } catch (error) {
+      console.error('Error adding SVG loader:', error);
+      if (!(error instanceof Error)) {
+        throw new TypeError('Error must be an instance of Error');
+      }
+      throw error;
+    }
 
     return config;
   },
