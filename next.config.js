80ff6f8b3875b2889484721e52d20a86e67a79e0
Classic Values <classicvaluesllc@gmail.com>
2024-07-04 18:15:42 -0700

    COMMIT

--- next.config.js
+++ next.config.js
@@ -4,15 +4,22 @@ module.exports = withSvgr({
   reactStrictMode: true,
   webpack(config, options) {
     try {
+      // Add SVG loader
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       });
+
+      // Additional configuration can be added here
     } catch (error) {
+      // Log the error and rethrow it if it's not an instance of Error
       console.error('Error adding SVG loader:', error);
+
       if (!(error instanceof Error)) {
+        console.error('The caught error is not an instance of Error:', error);
         throw new TypeError('Error must be an instance of Error');
       }
+
       throw error;
     }
 
