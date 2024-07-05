544b33f5811f6ade08f9b3db724427306a51cd87
Classic Values <classicvaluesllc@gmail.com>
2024-07-04 17:15:53 -0700

    COMMIT

--- src/components/pricing-table/index.tsx
+++ src/components/pricing-table/index.tsx
@@ -1,6 +1,6 @@
 import { tw } from 'twind';
-import Check from 'src/constants/svg/check.svg';
-import Button from 'src/components/button';
+import Check from '@/constants/svg/check.svg';
+import Button from '@/components/button';
 
 const features = [
   `Laboris nulla`,
